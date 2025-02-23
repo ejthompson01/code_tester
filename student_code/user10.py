903c8016c12d3dee2b6194c99d0828b005a454a3
Elizabeth Thompson <elizabeththompson@Elizabeths-MacBook-Air-4.local>
2025-02-23 16:26:12 -0500

    Review my code assignement

---
+++ student_code/user10.py
@@ -0,0 +1,13 @@
+# q1 adds 3 instead of 1, so incorrect output
+
+def q1(n: int) -> int:
+    return n+3
+
+def q2(n: int, m: int) -> int:
+    return n**2 - m
+
+def q3() -> int:
+    return 42
+
+def q4(n: int) -> tuple[int, str]:
+    return n+1, f'n^2 is {n**2}'
